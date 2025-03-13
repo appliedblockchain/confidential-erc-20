bd5f4ec7d9e2d0228d129259d3c653af49f3dd93
Ricardo Lopes <ricardo.lopes@appliedblockchain.com>
2025-03-13 10:49:48 +0000

    update ERC20OnlyOwnerAndRegulatorLargeBalances.sol

--- examples/ERC20OnlyOwnerAndRegulatorLargeBalances.sol
+++ examples/ERC20OnlyOwnerAndRegulatorLargeBalances.sol
@@ -4,19 +4,27 @@ pragma solidity ^0.8.22;
 import {ERC20} from "../ERC20/ERC20.sol";
 import {ERC20Permit} from "../ERC20/extensions/ERC20Permit.sol";
 
-// An example implementation that allows an owner to view their own balance but not the balances of others, but has a regulator that can view all balances
+// An example implementation that allows an owner to view their own balance but not the balances of others,
+// Regulator can view balances of accounts holding more than a threshold
 contract ERC20OnlyOwnerAndRegulatorLargeBalancesToken is ERC20, ERC20Permit {
     address public regulator;
+    uint256 private constant BALANCE_THRESHOLD = 10_000 Ether;
+
     constructor() ERC20("ERC20OnlyOwnerAndRegulatorLargeBalancesToken", "OOARLBT") ERC20Permit("ERC20OnlyOwnerAndRegulatorLargeBalancesToken") {
         regulator = msg.sender;
     }
 
     /**
      * @notice Override `balanceOf` to enforce privacy.
-     * @dev Only the balance owner or the admin can view the balance.
+     * @dev Only the balance owner can view balance. Admin can view balances greater than a threshold
      */
     function balanceOf(address account) public view override returns (uint256) {
-        require(msg.sender == account || regulator, "Unauthorized access to balance");
+        if (msg.sender == regulator) {
+            require(_balances[account] >= BALANCE_THRESHOLD, "Unauthorized access to balance");
+            return _balances[account];
+        }
+
+        require(msg.sender == account, "Unauthorized access to balance");
         return _balances[account];
     }
 }
