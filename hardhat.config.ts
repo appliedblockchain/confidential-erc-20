57b32324e3a7fc262f976188b3c807bad1dc23e9
Ricardo Lopes <ricardo.lopes@appliedblockchain.com>
2025-03-20 10:45:21 +0000

    erc3643 base

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,13 +10,27 @@ Config()
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.28',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: '0.8.28',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
-    },
+      // Version 0.8.17 is required for the ERC3643 contract
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   networks: {
     localhost: {
